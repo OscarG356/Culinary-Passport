1da84dd23be586b9dc19b62751be77cd5091ac46
Oscar Gutierrez <oscar.gutierrezr@udea.edu.co>
2024-04-20 23:12:00 -0500

    Se realizan modificaciones para iterar en los resultados obtenidos

--- Script/scripts.js
+++ Script/scripts.js
@@ -4,15 +4,14 @@ const api_ingredient = "http://themealdb.com/api/json/v1/1/filter.php?i=";
 const api_category = "http://themealdb.com/api/json/v1/1/filter.php?c=";
 const api_area = "http://themealdb.com/api/json/v1/1/filter.php?a=";
 
-//Selección del método de busqueda
 
 let selectElement = document.getElementById("menu");
 let search = document.getElementById("search");
 
-search.addEventListener("click", function () {
+search.addEventListener("click", async function () {
   let selectedValue = selectElement.value;
   let apiulr;
-
+  //Selección del método de busqueda
   switch (selectedValue) {
     case "method1":
       apiulr = api_name;
@@ -28,13 +27,48 @@ search.addEventListener("click", function () {
       break;
   }
 
+  //Validación de datos en la petición
   let textbox = document.getElementById("textbox").value;
   let url = apiulr + textbox;
+  if (textbox == "") {
+    alert("Please enter a value");
+    return;
+  }
   console.log(textbox);
   console.log(url);
 
-  consultarRecetas(url);
+  const data = await consultarRecetas(url);
+  console.log(data);
+  
+  //Creación de tarjetas de recetas
+  const results = document.getElementById("results");
+  results.setAttribute("class", "cards");
+
+  for(let meal of data.meals){
+    console.log(meal.strMeal);
+  }
+
+  /*
+  for(mels of data){
+    console.log(dato);
+    console.log(data);
+    const cards = document.createElement("div");
+    const img = document.createElement("img");
+    const title = document.createElement("h3");
+
+    img.setAttribute("src", dato.strMealThumb);
+    title.innerHTML = dato.strMeal;
+
+    cards.appendChild(img);
+    cards.appendChild(title);
+
+    results.appendChild(contenedor);
+  }
+  */
+  
 });
+
+
 
 //Función para consultar la API
 async function consultarRecetas(url) {
@@ -50,7 +84,7 @@ async function consultarRecetas(url) {
       infoerror.style.display = "none";
       results.style.display = "block";
     }
-    return response.data;
+    return response.data.meals;
   } catch (error) {
     console.error(`Falló esta vuelta: ${error}`);
     alert("API query failed");
